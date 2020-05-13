c759321febe87f17f6f9787861d3c2cd83a810ac
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-13 16:26:26 -0600

    Admin, agregando las columnas en admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+admin.site.register(Product,ProductAdmin)
 
 
