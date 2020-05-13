7b62e3882d6be883c56931aa25c2fc9ec6c008a4
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-13 16:37:02 -0600

    Views, poniendo la lista de productos en la primera pagina

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
 # / productos - index
 # Uniform Resource Locator (Adress)
 def index(request):
+    products = Product.objets.all()
     return HttpResponse('Hello World')
 
 
