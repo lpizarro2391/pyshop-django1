38eb75649ac9fdd90e4ae4cef467d3c524542097
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-13 16:50:03 -0600

    Vistas agregando productos a la pagina principal

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Product
-# / productos - index
-# Uniform Resource Locator (Adress)
+
+
 def index(request):
-    products = Product.objets.all()
-    return HttpResponse('Hello World')
+    products = Product.objects.all()
+    return render(request, 'index.html')
 
 
 def new(request):
