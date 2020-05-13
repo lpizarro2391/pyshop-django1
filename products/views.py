817f59447b267d9ad16c384c9abaed3812cadde9
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-13 16:54:23 -0600

    Vista reemplazando prodcutos por la lista real

--- products/views.py
+++ products/views.py
@@ -5,9 +5,9 @@ from .models import Product
 
 def index(request):
     products = Product.objects.all()
-    return render(request, 'index.html')
+    return render(request, 'index.html',
+    {'products': products })
 
 
 def new(request):
     return HttpResponse('New Products')
-
