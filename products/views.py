97d1dee99f7ff18a1b7d6c22bce554c958f47fa6
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-14 09:34:18 -0600

    index, jalar los productso de la base de datos

--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ from .models import Product
 def index(request):
     products = Product.objects.all()
     return render(request, 'index.html',
-    {'products': products })
+                  {'products': products })
 
 
 def new(request):
